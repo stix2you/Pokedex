70407952327af92a6613946312a92615c0fe9616
Doug Hill <stix2you@gmail.com>
2023-12-01 12:49:30 -0600

    created variable array for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-alert('Hello world');
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] }
+    { name: 'Ivysaur', height: 1.0, types: ['grass', 'poison'] }
+    { name: 'Venusaur', height: 2.0, types: ['grass', 'poison'] }
+    { name: 'Charmander', height: 0.6, types: ['fire'] }
+    { name: 'Charmeleon', height: 1.1, types: ['fire'] }
+    { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] }
+    { name: 'Squirtle', height: 0.5, types: ['water'] }
+    { name: 'Wartortle', height: 1.0, types: ['water'] }
+    { name: 'Blastoise', height: 1.6, types: ['water'] }
+    { name: 'Caterpie', height: 0.3, types: ['bug'] }
+];
