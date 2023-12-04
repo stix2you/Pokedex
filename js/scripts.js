3453b631b0925c03eb648f6493475fc9d655c8e5
Doug Hill <stix2you@gmail.com>
2023-12-04 15:40:29 -0600

    Added FOR loop to display names and heights

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] }
-    { name: 'Ivysaur', height: 1.0, types: ['grass', 'poison'] }
-    { name: 'Venusaur', height: 2.0, types: ['grass', 'poison'] }
-    { name: 'Charmander', height: 0.6, types: ['fire'] }
-    { name: 'Charmeleon', height: 1.1, types: ['fire'] }
-    { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] }
-    { name: 'Squirtle', height: 0.5, types: ['water'] }
-    { name: 'Wartortle', height: 1.0, types: ['water'] }
-    { name: 'Blastoise', height: 1.6, types: ['water'] }
-    { name: 'Caterpie', height: 0.3, types: ['bug'] }
-];
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+    { name: 'Ivysaur', height: 1.0, types: ['grass', 'poison'] },
+    { name: 'Venusaur', height: 2.0, types: ['grass', 'poison'] },
+    { name: 'Charmander', height: 0.6, types: ['fire'] },
+    { name: 'Charmeleon', height: 1.1, types: ['fire'] },
+    { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
+    { name: 'Squirtle', height: 0.5, types: ['water'] },
+    { name: 'Wartortle', height: 1.0, types: ['water'] },
+    { name: 'Blastoise', height: 1.6, types: ['water'] },
+    { name: 'Caterpie', height: 0.3, types: ['bug'] },
+]
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ", height: " + pokemonList[i].height + " --- ");
+  }
