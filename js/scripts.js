72b5128d3f9f99c9c1d99d8ab56f3221b1dbcdb2
Doug Hill <stix2you@gmail.com>
2023-12-04 16:15:52 -0600

    Added "WOW" msg to listing with for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Define and fill Pokemon List Array
 let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
     { name: 'Ivysaur', height: 1.0, types: ['grass', 'poison'] },
@@ -11,7 +12,11 @@ let pokemonList = [
     { name: 'Caterpie', height: 0.3, types: ['bug'] },
 ]
 
-// Loop to display names and heights next to each name
+// Loop to display names and heights next to each name, include "wow" tag if height over 1.0
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "), ");
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+    if (pokemonList[i].height >= 1.0) {
+        document.write(" -- Wow that's big!")
+    };
+    document.write("<br>");
   }
