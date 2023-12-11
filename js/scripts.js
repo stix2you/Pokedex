dd6af5ef6434244591b8712e82a4e93ffa12f8bd
Doug Hill <stix2you@gmail.com>
2023-12-11 11:35:34 -0600

    Attempted to add new functions, could not do it

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 //  Define new variable pokemonRepository   and   assign to Pokemon List Array
 let pokemonRepository = (function () {
     let pokemonList = [
@@ -13,21 +12,52 @@ let pokemonRepository = (function () {
         { name: 'Blastoise', height: 1.6, types: ['water'] },
         { name: 'Caterpie', height: 0.3, types: ['bug'] },
     ];
-
+    
     //separately defined functions for 'add' and 'getAll'
     function add(pokemon) {
-        pokemonList.push(pokemon);
+
+        // let isValid = Object.keys(pokemon);
+        // console.log(isValid);
+        // if  (isValid = ["name:", "height:", "types:"]) {
+        //     pokemonList.push(pokemon);
+        //   } else {
+        //     console.error("Did not add item: " + pokemon + " ....Items must be objects to add to repository");
+        //   };
+        
+        //Object.keys(pokemon).forEach(function(property) { console.log(pokemon[property]); } );
+
+        // if-else to check passed data is an object, ELSE refuse to add and error msg to console
+        if (typeof pokemon === 'object')  {
+            pokemonList.push(pokemon);
+          } else {
+            console.error("Did not add item: " + pokemon + " ....Items must be objects to add to repository");
+          };
     }
+
 
     function getAll() {
         return pokemonList;
     }
 
+    // function getSpecific(specificPokemon) {
+    //     let specific = pokemonList.filter(iteration => iteration === specificPokemon);
+    //     return specific;
+    // }
+
     return {
         add: add,  // key and value are same
-        getAll: getAll    // key and value are same
+        getAll: getAll,    // key and value are same
+        // getSpecific: getSpecific   // key and value are same
     };
 })();
+
+// Test adding an object and a non-object variable
+let newPokemonObject = { name: 'Metapod', height: 0.7, types: ['bug'] };
+pokemonRepository.add(newPokemonObject);
+
+let newPokemonString = 'STRING name: Metapod, height: 0.7, types: [bug]';
+pokemonRepository.add(newPokemonString);
+
 
 // loop to display pokemonList array within the pokemonRepository
 pokemonRepository.getAll().forEach(function(pokemonRepository) {
